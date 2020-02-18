b8f4cd721bfa7e9c225ee86e64f2648d3347f6e2
unknown <chrisrisvik@gmail.com>
2020-02-18 18:22:07 +0100

    major upgrades to cloning playbook and a script to deploy

---
+++ deploy_vm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# script to deploy vm in center that calls on ansible
+# must be located in the same folder as ansible
+export ANSIBLE_LOG_PATH=./ansible.log
+OS=$1
+VM_NAME=$2
+TEMPLATE_NAME=""
+
+if [ $OS == 'kali' ]; then
+    #printf 'selected os %s\n', $1
+    TEMPLATE_NAME='kali-2020-template'
+elif [ $OS == 'ubuntu' ]; then
+    TEMPLATE_NAME='ubuntu-1804-template'
+elif [ $OS == 'windows' ]; then
+    TEMPLATE_NAME='w10-template'
+fi
+
+# prefix vm name with username
+USER_NAME=$(whoami)
+VM_NAME=$USER_NAME'-'$VM_NAME
+
+printf 'Available operating systems:\n'
+printf 'kali, ubuntu, w10\n\n'
+printf 'Deploying VM with OS %s and name %s.\n' $OS $VM_NAME
+#ansible-playbook -i inventory vcenter_vm_playbook.yml --extra-vars '{"template_name":"'$TEMPLATE_NAME'","vm_name":"'$VM_NAME'","folder_path":"'$USER_NAME'"}'
+
+# print post install messages
+IP=$(grep -m 1 'IP:' ansible.log | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])')
+printf '\nYou can now access your VM %s with RDP to Windows, or SSH to Linux.\n' $VM_NAME
+printf 'Access your VM with:\n'
+printf 'ssh %s@%s\n\n' $OS $IP
+printf 'SSH access is only possible with your private key.\n'
+printf 'Default credentials for Windows is Administrator/Welcome1\n'
+printf 'Happy hacking!\n'
